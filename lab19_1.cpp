e1e3619655a7ba207d22e4b029a542ec5784ccce
kawin  choosin <kawin_chusin@cmu.ac.th>
2023-02-21 01:42:12 +0700

    edit

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
-
+#include<cstring>
 using namespace std;
 
 char score2grade(int score){
@@ -20,42 +20,63 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(string f,vector<string> n, vector<int> s,vector<char> g){
-    char format[]=" %s %d %s";
+void importDataFromFile(string f,vector<string> &n, vector<int> &s,vector<char> &g){
     string textline;
+    char format[] = "%[^:]: %d %d %d";
     ifstream source;
     source.open (f);
-    for(int i=0;getline(source,textline);i++){
-        sscanf(textline,format,n,&s,g)
-        n[i]=n;
-        s[i]=s;
-        g[i]=g;
-    }
+    int a,b,c;
+    char name[100];
+    while(getline(source,textline)){
+        sscanf(textline.c_str(),format,name,&a,&b,&c);
+        n.push_back(name);
+        s.push_back(a+b+c);
+        g.push_back(score2grade(a+b+c));
+        }
 }
 
-void getCommand(string command,string key){
-    cout<<"Please input your command: ";
-    cin>>command>>key;
+void getCommand(string &command,string &key){
+        string t;
+        cout<<"Please input your command: ";
+        getline(cin,t);
+        int start =0;
+        int end=t.find_first_of(" ");
+        command=t.substr(start,end-start);
+        start=end+1;
+        key=t.substr(start,t.find_last_of(" ",start)-start);
 }
 
 void searchName(vector<string> n, vector<int> s,vector<char> g,string key){
-     cout << "---------------------------------\n";
-    for(int i=0;toUpperStr(n[i])==key;i++){
-        cout<<n[i]<<" 's score = "<<s[i];
-        cout<<n[i]<<" 's grade = "<<g[i];
-    }
-     cout << "---------------------------------\n";
+            int count=0;
+            cout << "---------------------------------\n";
+            for(unsigned int i=0;i<n.size();i++){
+                if(toUpperStr(n[i])==key){
+                cout<<n[i]<<"'s score = "<<s[i]<<endl;
+                cout<<n[i]<<"'s grade = "<<g[i]<<endl;
+                count++;
+                }
+            }
+            if(count==0){
+                    cout<<"Cannot found."<<endl;
+                }
+
+            cout << "---------------------------------\n";
 }
 
 void searchGrade(vector<string> n, vector<int> s,vector<char> g,string key){
-     cout << "---------------------------------\n";
-    for(int i=0;toUpperStr(g[i])==key;i++){
-        cout<<n[i]<<" ("<<i<<") \n";
-    }
-     cout << "---------------------------------\n";
+            int count=0;
+            cout << "---------------------------------\n";
+            for(unsigned int i=0;i<n.size();i++){
+                if(g[i] == *key.c_str()){
+                     cout<<n[i]<<" ("<<s[i]<<") \n";
+                     count ++;
+                }
+            }
+            if(count==0){
+                    cout<<"Cannot found.";
+                }
+            cout << "---------------------------------\n";
 }
-
-
 
 
 int main(){
